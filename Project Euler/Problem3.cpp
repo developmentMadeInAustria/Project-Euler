40fa60c810a3496076ff37d1330da764ac942347
Dominik Gail <dominik.gail@Dominiks-MacBook-Pro.local>
2018-11-22 19:47:20 +0100

    Third Problem

---
+++ Project Euler/Problem3.cpp
@@ -0,0 +1,9 @@
+//
+//  Problem3.cpp
+//  Project Euler
+//
+//  Created by Dominik Gail on 22.11.18.
+//  Copyright © 2018 Dominik Gail. All rights reserved.
+//
+
+#include "Problem3.hpp"
---
+++ Project Euler/Problem3.hpp
@@ -0,0 +1,14 @@
+//
+//  Problem3.hpp
+//  Project Euler
+//
+//  Created by Dominik Gail on 22.11.18.
+//  Copyright © 2018 Dominik Gail. All rights reserved.
+//
+
+#ifndef Problem3_hpp
+#define Problem3_hpp
+
+#include <stdio.h>
+
+#endif /* Problem3_hpp */
