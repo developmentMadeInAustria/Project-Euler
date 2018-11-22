549fe70d4380b2750950c037eac725b9f473cac2
Dominik Gail <dominik.gail@gmx.net>
2018-11-22 17:19:31 +0100

    Initial Commit

---
+++ Project Euler/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project Euler
+//
+//  Created by Dominik Gail on 22.11.18.
+//  Copyright © 2018 Dominik Gail. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
