d55371ea94a207403c4a824b7691ae5592e8662f
Dominik Gail <dominik.gail@Dominiks-MacBook-Pro.local>
2018-11-24 20:42:30 +0100

    Problem Six

---
+++ Project Euler/Problem6.hpp
@@ -0,0 +1,14 @@
+//
+//  Problem6.hpp
+//  Project Euler
+//
+//  Created by Dominik Gail on 24.11.18.
+//  Copyright © 2018 Dominik Gail. All rights reserved.
+//
+
+#ifndef Problem6_hpp
+#define Problem6_hpp
+
+#include <stdio.h>
+
+#endif /* Problem6_hpp */
