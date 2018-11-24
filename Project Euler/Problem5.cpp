47e81c5f08c7ba92a491b9996c73825d50058273
Dominik Gail <dominik.gail@Dominiks-MacBook-Pro.local>
2018-11-24 20:13:31 +0100

    Problem Five

---
+++ Project Euler/Problem5.cpp
@@ -0,0 +1,9 @@
+//
+//  Problem5.cpp
+//  Project Euler
+//
+//  Created by Dominik Gail on 24.11.18.
+//  Copyright © 2018 Dominik Gail. All rights reserved.
+//
+
+#include "Problem5.hpp"
---
+++ Project Euler/Problem5.hpp
@@ -0,0 +1,14 @@
+//
+//  Problem5.hpp
+//  Project Euler
+//
+//  Created by Dominik Gail on 24.11.18.
+//  Copyright © 2018 Dominik Gail. All rights reserved.
+//
+
+#ifndef Problem5_hpp
+#define Problem5_hpp
+
+#include <stdio.h>
+
+#endif /* Problem5_hpp */
