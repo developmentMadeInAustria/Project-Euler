501185085c3561be2b007e8c1a4bec4cb86693e9
Dominik Gail <dominik.gail@Dominiks-MacBook-Pro.local>
2018-11-24 19:53:33 +0100

    Fourth ProblemFourth Problem

---
+++ Project Euler/Problem4.cpp
@@ -0,0 +1,9 @@
+//
+//  Problem4.cpp
+//  Project Euler
+//
+//  Created by Dominik Gail on 22.11.18.
+//  Copyright © 2018 Dominik Gail. All rights reserved.
+//
+
+#include "Problem4.hpp"
---
+++ Project Euler/Problem4.hpp
@@ -0,0 +1,14 @@
+//
+//  Problem4.hpp
+//  Project Euler
+//
+//  Created by Dominik Gail on 22.11.18.
+//  Copyright © 2018 Dominik Gail. All rights reserved.
+//
+
+#ifndef Problem4_hpp
+#define Problem4_hpp
+
+#include <stdio.h>
+
+#endif /* Problem4_hpp */
