7a3438fe24fcabfb724ea63b0b2c3e90700a64fa
Dominik Gail <dominik.gail@Dominiks-MacBook-Pro.local>
2018-11-29 09:22:14 +0100

    Problem Seven

---
+++ Project Euler/Problem7.cpp
@@ -0,0 +1,9 @@
+//
+//  Problem7.cpp
+//  Project Euler
+//
+//  Created by Dominik Gail on 29.11.18.
+//  Copyright © 2018 Dominik Gail. All rights reserved.
+//
+
+#include "Problem7.hpp"
---
+++ Project Euler/Problem7.hpp
@@ -0,0 +1,14 @@
+//
+//  Problem7.hpp
+//  Project Euler
+//
+//  Created by Dominik Gail on 29.11.18.
+//  Copyright © 2018 Dominik Gail. All rights reserved.
+//
+
+#ifndef Problem7_hpp
+#define Problem7_hpp
+
+#include <stdio.h>
+
+#endif /* Problem7_hpp */
