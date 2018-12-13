5131ca20a253f55e1457c6b1b61e1620b2475bc6
Dominik Gail <dominik.gail@Dominiks-MacBook-Pro.local>
2018-12-13 21:07:25 +0100

    Problem 8

---
+++ Project Euler/Problem8.cpp
@@ -0,0 +1,9 @@
+//
+//  Problem8.cpp
+//  Project Euler
+//
+//  Created by Dominik Gail on 08.12.18.
+//  Copyright © 2018 Dominik Gail. All rights reserved.
+//
+
+#include "Problem8.hpp"
---
+++ Project Euler/Problem8.hpp
@@ -0,0 +1,14 @@
+//
+//  Problem8.hpp
+//  Project Euler
+//
+//  Created by Dominik Gail on 08.12.18.
+//  Copyright © 2018 Dominik Gail. All rights reserved.
+//
+
+#ifndef Problem8_hpp
+#define Problem8_hpp
+
+#include <stdio.h>
+
+#endif /* Problem8_hpp */
