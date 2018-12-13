85be7e2df2a7f8fdc3c5ba354c41f6b596e0c6d3
Dominik Gail <dominik.gail@Dominiks-MacBook-Pro.local>
2018-12-13 21:33:01 +0100

    Problem 9

---
+++ Project Euler/Problem9.cpp
@@ -0,0 +1,9 @@
+//
+//  Problem9.cpp
+//  Project Euler
+//
+//  Created by Dominik Gail on 13.12.18.
+//  Copyright © 2018 Dominik Gail. All rights reserved.
+//
+
+#include "Problem9.hpp"
---
+++ Project Euler/Problem9.hpp
@@ -0,0 +1,14 @@
+//
+//  Problem9.hpp
+//  Project Euler
+//
+//  Created by Dominik Gail on 13.12.18.
+//  Copyright © 2018 Dominik Gail. All rights reserved.
+//
+
+#ifndef Problem9_hpp
+#define Problem9_hpp
+
+#include <stdio.h>
+
+#endif /* Problem9_hpp */
